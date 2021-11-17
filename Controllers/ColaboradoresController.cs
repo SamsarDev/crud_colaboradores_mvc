41ad71aa2f0628792c6cc055793587961dd92537
Samuel Sarmientos <67299065+SamsarDev@users.noreply.github.com>
2021-11-16 20:17:26 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/ColaboradoresController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCCrud.Models;
+
+namespace MVCCrud.Controllers
+{
+    public class ColaboradoresController : Controller
+    {
+        // GET: Colaboradores
+        public ActionResult Index()
+        {
+            List<ListColaboradoresModel> lst;
+
+            using (Crud_App_EspEntities db = new Crud_App_EspEntities())
+            {
+                var lst = (from d in db.Colaboradores
+                           select new ListColaboradoresModel
+                           {
+                               ID_Colaborador = d.ID_Colaborador,
+                               Nombres = d.Nombres,
+                               Apellidos = d.Apellidos,
+                               FechaNacimiento = d.FechaNacimiento,
+                               EstadoCivil = d.EstadoCivil,
+                               GradoAcademico = d.GradoAcademico,
+                               Direccion = d.Direccion
+                           }).ToList();
+            }
+
+            return View(lst);
+        }
+    }
+}
---
+++ Models/ViewModels/ListColaboradoresModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCCrud.Models
+{
+    public class ListColaboradoresModel
+    {
+        [Key]
+        public int ID_Colaborador { get; set; }
+        [Required]
+        public string Nombres { get; set; }
+        [Required]
+        public string Apellidos { get; set; }
+        [Required]
+        public DateTime FechaNacimiento { get; set; }
+        public string EstadoCivil { get; set; }
+        public string GradoAcademico { get; set; }
+        public string Direccion { get; set; }
+
+    }
+}
